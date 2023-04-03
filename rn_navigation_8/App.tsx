78c90fc4400551eaa24d43c0c8c671168717c406
duycuongit <118497439+duycuongit@users.noreply.github.com>
2023-04-03 21:59:18 +0700

    learn navigation

--- rn_navigation_8/App.tsx
+++ rn_navigation_8/App.tsx
@@ -1,45 +1,76 @@
 // In App.js in a new project
 
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import { Button, View, Text, TextInput } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 function HomeScreen({ route, navigation }) {
-  const { itemId } = route.params;
+  const { itemId, otherParam } = route.params;
+  React.useEffect(() => {
+    if (route.params?.post) {
+      // Post updated, do something with `route.params.post`
+      // For example, send the post to the server
+    }
+  }, [route.params?.post]);
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
-      <Text>Initial param: itemId - {itemId}</Text>
+      <Text>itemId: {JSON.stringify(itemId)}</Text>
+      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
       <Button
         title="Go to Details"
         onPress={() => {
           /* 1. Navigate to the Details route with params */
           navigation.navigate('Details', {
-            itemId: 86,
-            otherParam: 'anything you want here',
+            item: 86,
+            param: 'anything you want here',
           });
         }}
       />
+      <Button
+        title="Update param"
+        onPress={() =>
+          navigation.setParams({
+            itemId: Math.floor(Math.random() * 100),
+            otherParam: 'set param'
+          })
+        }
+      />
+      <Button
+        title="Update param by push"
+        onPress={() =>
+          navigation.push('Home', {
+            itemId: 1,
+            otherParam: 'pushed',
+          })
+        }
+      />
+       <Button
+        title="Create post"
+        onPress={() => navigation.navigate('CreatePost')}
+      />
+      <Text style={{ margin: 10 }}>Post: {route.params?.post}</Text>
     </View>
   );
 }
 
 function DetailsScreen({ route, navigation }) {
   /* 2. Get the param */
-  const { itemId, otherParam } = route.params;
+  const { item, param } = route.params;
+  
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
-      <Text>itemId: {JSON.stringify(itemId)}</Text>
-      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+      <Text>item: {JSON.stringify(item)}</Text>
+      <Text>param: {JSON.stringify(param)}</Text>
       <Button
         title="Go to Details... again"
         onPress={() =>
           navigation.push('Details', {
-            itemId: Math.floor(Math.random() * 100),
-            otherParam: 'thing i want',
+            item: Math.floor(Math.random() * 100),
+            param: 'thing i want',
           })
         }
       />
@@ -53,14 +84,42 @@ function DetailsScreen({ route, navigation }) {
   );
 }
 
+function CreatePostScreen({ navigation, route }) {
+  const [postText, setPostText] = React.useState('');
+
+  return (
+    <>
+      <TextInput
+        multiline
+        placeholder="What's on your mind?"
+        style={{ height: 200, padding: 10, backgroundColor: 'white' }}
+        value={postText}
+        onChangeText={setPostText}
+      />
+      <Button
+        title="Done"
+        onPress={() => {
+          // Pass and merge params back to home screen
+          navigation.navigate({
+            name: 'Home',
+            params: { post: postText, itemId: 0, otherParam: 'param From Create Post' },
+            merge: true,
+          });
+        }}
+      />
+    </>
+  );
+}
+
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Overview' }} initialParams={{ itemId: 42 }}/>
-        <Stack.Screen name="Details" component={DetailsScreen}  />
+      <Stack.Navigator initialRouteName="Home" mode="modal">
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Home page' }} initialParams={{ itemId: 42, otherParam: 'not push'}}/>
+        <Stack.Screen name="Details" component={DetailsScreen} options={{ title: 'Detail page' }} />
+        <Stack.Screen name="CreatePost" component={CreatePostScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
